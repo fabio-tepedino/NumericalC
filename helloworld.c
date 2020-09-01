df9de29300783be499e1c7d8d022ad7ad3dd3f7c
fabiotp <37007510+fabiotp@users.noreply.github.com>
2020-09-01 02:51:07 -0300

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -2,5 +2,18 @@
 int main()
 {
 printf("Hello World\n");
+  
+float this_is_a_number, total;
+int i;
+total = 0;
+
+for(i = 0; i < 5; i++)
+{
+  printf("Please enter a number:\n");
+  scanf("%f", &this_is_a_number);
+  total = total + this_is_a_number;
+}
+printf("Total Sum is = %f\n", total);
+  
 return 0;
 }
